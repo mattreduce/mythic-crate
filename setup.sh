75ebbbdb79c74723f0dab0ceaa8c505cdf3bb097
Matthew Conway <mattreduce@users.noreply.github.com>
2021-06-22 16:56:40 -0700

    Update setup.sh, Mythic v2.2 shipped :tada:

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 git clone https://github.com/its-a-feature/Mythic.git
 cd Mythic
-git checkout -t origin/2.2-updates
 
 sudo ./install_docker_ubuntu.sh
 
-sudo -E ./mythic-cli install github https://github.com/MythicC2Profiles/http.git
-sudo -E ./mythic-cli install github https://github.com/MythicAgents/apfell -f
-sudo -E ./mythic-cli install github https://github.com/MythicAgents/poseidon -f
+sudo -E ./mythic-cli install github https://github.com/MythicC2Profiles/http
+sudo -E ./mythic-cli install github https://github.com/MythicAgents/apfell
+sudo -E ./mythic-cli install github https://github.com/MythicAgents/poseidon
 sudo -E ./mythic-cli mythic start
