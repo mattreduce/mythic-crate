7c0dc5968124229354a18e6fef4c9de52ef7a104
Matthew Conway <mattreduce@users.noreply.github.com>
2021-07-30 15:34:32 -0700

    Install dependencies for Mythic scripting

--- setup.sh
+++ setup.sh
@@ -6,11 +6,18 @@ sudo apt-get upgrade -y
 sudo timedatectl set-ntp no
 sudo apt-get install ntp -y
 
+# Install dependencies for Mythic scripting
+sudo apt-get install python3-pip -y
+pip3 install mythic
+
+# Get the latest Mythic from GitHub
 git clone https://github.com/its-a-feature/Mythic.git
 cd Mythic
 
+# Install Docker and Docker Compose
 sudo ./install_docker_ubuntu.sh
 
+# Install a C2 Profile and some Payloads, start everything up
 sudo -E ./mythic-cli install github https://github.com/MythicC2Profiles/http
 sudo -E ./mythic-cli install github https://github.com/MythicAgents/apfell
 sudo -E ./mythic-cli install github https://github.com/MythicAgents/poseidon
